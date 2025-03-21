bcf766f5db3918ac9d3daebbc1fde56ad389cd55
GitHub Action <action@github.com>
2025-03-21 09:19:45 +0000

    Patch versiyon arttırıldı: v0.1.6

--- src/modules/layout/templates/footer/index.tsx
+++ src/modules/layout/templates/footer/index.tsx
@@ -174,7 +174,7 @@ export default async function Footer() {
           <div className="flex flex-col">
             <p>© {new Date().getFullYear()} Animal Wishbox.</p>
             <p>Tüm hakları saklıdır.</p>
-            <p>v0.1.5</p>
+            <p>v0.1.6</p>
 
             <ul className="mt-2">
               <li>
