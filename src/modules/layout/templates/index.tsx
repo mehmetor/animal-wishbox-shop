4e762ab3261fef6e32889659df3e397d17e65614
Mehmet <mehmetakiforakci@gmail.com>
2025-07-02 18:38:15 +0300

    Footer bileşeninde stil güncellemeleri yapıldı; alt bilgi düzeni iyileştirildi, gereksiz kodlar kaldırıldı ve MedusaCTA bileşeni eklendi. Layout bileşeninde de küçük düzenlemeler gerçekleştirildi.

--- src/modules/layout/templates/index.tsx
+++ src/modules/layout/templates/index.tsx
@@ -1,21 +1,18 @@
-import React from "react"
+import React from "react";
 
-import Footer from "@modules/layout/templates/footer"
-import Nav from "@modules/layout/templates/nav"
-
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import Footer from "@modules/layout/templates/footer";
+import Nav from "@modules/layout/templates/nav";
 
 const Layout: React.FC<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }> = ({ children }) => {
   return (
     <div>
       <Nav />
       <main className="relative">{children}</main>
       <Footer />
-      <SpeedInsights />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
